d075f98f8cad430025d10c514cdf577beae97417
Riaan Taveras <riaantaveras@gmail.com>
2019-12-04 04:55:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,9 +30,21 @@ def using_insert (array, element)
   array.insert(4, element)
 end
 
+def using_flatten (array)
+  array.flatten
+end
+  
+  
 def using_uniq (array)
   puts array
 end
 
+def using_delete (array, string)
+  array.delete(string)
+end
+
+def using_delete_at (array, int)
+  array.delete(int)
+end
 
 
